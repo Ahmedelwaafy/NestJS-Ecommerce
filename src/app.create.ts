0c773176a6626328508826216759876dadda9499
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2025-02-13 06:52:53 +0200

    add swagger configuration

---
+++ src/app.create.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function createApp(app: INestApplication) {
+  /**
+   * swagger configuration
+   */
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJs Ecommerce API')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+    )
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  // Instantiate Document
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+}
