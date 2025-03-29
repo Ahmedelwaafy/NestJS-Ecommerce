0c0888707324f5516b10e19a49bae01f4ea10b99
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2025-03-29 23:24:52 +0200

    finish auth-model

--- git-automation.sh
+++ git-automation.sh
@@ -5,16 +5,16 @@ git add -A
 git commit -m "$1"
 
 # Step 2: Push to the current branch
-git push origin profile-model
+git push origin auth-model
 
 # Step 3: Checkout development branch, pull updates, merge, and push
 git checkout development
 git pull origin development
-git merge profile-model
+git merge auth-model
 git push origin development
-# Step 4: Switch back to profile-model branch
-git checkout profile-model
+# Step 4: Switch back to auth-model branch
+git checkout auth-model
 
 
-# Step 5: ./git-automation.sh "finish user profile model"
+# Step 5: ./git-automation.sh "finish auth-model"
 #git merge development
