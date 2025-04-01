6cba13f33662f51bb7d390e325c15c44eace6e9c
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2025-04-01 10:32:53 +0200

    finish coupon-model

--- git-automation.sh
+++ git-automation.sh
@@ -5,16 +5,16 @@ git add -A
 git commit -m "$1"
 
 # Step 2: Push to the current branch
-git push origin brand-model
+git push origin coupon-model
 
 # Step 3: Checkout development branch, pull updates, merge, and push
 git checkout development
 git pull origin development
-git merge brand-model
+git merge coupon-model
 git push origin development
-# Step 4: Switch back to brand-model branch
-git checkout brand-model
+# Step 4: Switch back to coupon-model branch
+git checkout coupon-model
 
 
-# Step 5: ./git-automation.sh "finish brand-model"
+# Step 5: ./git-automation.sh "finish coupon-model"
 #git merge development
