d1df674308e98a993ef1ff9d5d7d944839988cda
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2025-04-01 08:19:24 +0200

    finish brand-model

--- git-automation.sh
+++ git-automation.sh
@@ -5,16 +5,16 @@ git add -A
 git commit -m "$1"
 
 # Step 2: Push to the current branch
-git push origin sub-categories-model
+git push origin brand-model
 
 # Step 3: Checkout development branch, pull updates, merge, and push
 git checkout development
 git pull origin development
-git merge sub-categories-model
+git merge brand-model
 git push origin development
-# Step 4: Switch back to sub-categories-model branch
-git checkout sub-categories-model
+# Step 4: Switch back to brand-model branch
+git checkout brand-model
 
 
-# Step 5: ./git-automation.sh "finish sub-categories-model"
+# Step 5: ./git-automation.sh "finish brand-model"
 #git merge development
